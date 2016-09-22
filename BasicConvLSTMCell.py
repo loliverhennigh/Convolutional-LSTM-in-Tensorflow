1246d2bb22c2c50b592e2f43de536e7c9b02ff57
oliver <loliverhennigh101@gmail.com>
2016-09-22 12:16:38 -0400

    updated yay!

--- BasicConvLSTMCell.py
+++ BasicConvLSTMCell.py
@@ -1,49 +1,18 @@
 
 import tensorflow as tf
 
-class RNNCell(object):
-  """Abstract object representing an RNN cell.
-  The definition of cell in this package differs from the definition used in the
-  literature. In the literature, cell refers to an object with a single scalar
-  output. The definition in this package refers to a horizontal array of such
-  units.
-  An RNN cell, in the most abstract setting, is anything that has
-  a state and performs some operation that takes a matrix of inputs.
-  This operation results in an output matrix with `self.output_size` columns.
-  If `self.state_size` is an integer, this operation also results in a new
-  state matrix with `self.state_size` columns.  If `self.state_size` is a
-  tuple of integers, then it results in a tuple of `len(state_size)` state
-  matrices, each with the a column size corresponding to values in `state_size`.
-  This module provides a number of basic commonly used RNN cells, such as
-  LSTM (Long Short Term Memory) or GRU (Gated Recurrent Unit), and a number
-  of operators that allow add dropouts, projections, or embeddings for inputs.
-  Constructing multi-layer cells is supported by the class `MultiRNNCell`,
-  or by calling the `rnn` ops several times. Every `RNNCell` must have the
-  properties below and and implement `__call__` with the following signature.
+class ConvRNNCell(object):
+  """Abstract object representing an Convolutional RNN cell.
   """
 
   def __call__(self, inputs, state, scope=None):
     """Run this RNN cell on inputs, starting from the given state.
-    Args:
-      inputs: `2-D` tensor with shape `[batch_size x input_size]`.
-      state: if `self.state_size` is an integer, this should be a `2-D Tensor`
-        with shape `[batch_size x self.state_size]`.  Otherwise, if
-        `self.state_size` is a tuple of integers, this should be a tuple
-        with shapes `[batch_size x s] for s in self.state_size`.
-      scope: VariableScope for the created subgraph; defaults to class name.
-    Returns:
-      A pair containing:
-      - Output: A `2-D` tensor with shape `[batch_size x self.output_size]`.
-      - New state: Either a single `2-D` tensor, or a tuple of tensors matching
-        the arity and shapes of `state`.
     """
     raise NotImplementedError("Abstract method")
 
   @property
   def state_size(self):
     """size(s) of state(s) used by this cell.
-    It can be represented by an Integer, a TensorShape or a tuple of Integers
-    or TensorShapes.
     """
     raise NotImplementedError("Abstract method")
 
@@ -58,15 +27,16 @@ class RNNCell(object):
       batch_size: int, float, or unit Tensor representing the batch size.
       dtype: the data type to use for the state.
     Returns:
-      If `state_size` is an int or TensorShape, then the return value is a
-      `N-D` tensor of shape `[batch_size x state_size]` filled with zeros.
-      If `state_size` is a nested list or tuple, then the return value is
-      a nested list or tuple (of the same structure) of `2-D` tensors with
-    the shapes `[batch_size x s]` for each s in `state_size`.
+      tensor of shape '[batch_size x shape[0] x shape[1] x num_features]
+      filled with zeros
     """
-    raise NotImplementedError("not implimented for conv lstm")
+    
+    shape = self.shape 
+    num_features = self.num_features
+    zeros = tf.zeros([batch_size, shape[0], shape[1], num_features * 2]) 
+    return zeros
 
-class BasicConvLSTMCell(RNNCell):
+class BasicConvLSTMCell(ConvRNNCell):
   """Basic Conv LSTM recurrent network cell. The
   """
 
---
+++ main_conv.py
@@ -0,0 +1,187 @@
+
+import os.path
+import time
+
+import numpy as np
+import tensorflow as tf
+import cv2
+
+import bouncing_balls as b
+import layer_def as ld
+import BasicConvLSTMCell
+
+FLAGS = tf.app.flags.FLAGS
+
+tf.app.flags.DEFINE_string('train_dir', './checkpoints/train_store_conv',
+                            """dir to store trained net""")
+tf.app.flags.DEFINE_integer('hidden_size', 20,
+                            """size of hidden layer""")
+tf.app.flags.DEFINE_integer('seq_length', 5,
+                            """size of hidden layer""")
+tf.app.flags.DEFINE_integer('max_step', 200000,
+                            """max num of steps""")
+tf.app.flags.DEFINE_float('keep_prob', .8,
+                            """for dropout""")
+tf.app.flags.DEFINE_float('lr', .001,
+                            """for dropout""")
+tf.app.flags.DEFINE_integer('batch_size', 16,
+                            """batch size for training""")
+tf.app.flags.DEFINE_float('weight_init', .1,
+                            """weight init for fully connected layers""")
+
+fourcc = cv2.cv.CV_FOURCC('m', 'p', '4', 'v') 
+
+def generate_bouncing_ball_sample(batch_size, seq_length, shape, num_balls):
+  dat = np.zeros((batch_size, seq_length, shape, shape, 3))
+  for i in xrange(batch_size):
+    dat[i, :, :, :, :] = b.bounce_vec(32, num_balls, seq_length)
+  return dat 
+
+def train():
+  """Train ring_net for a number of steps."""
+  with tf.Graph().as_default():
+    # make inputs
+    x = tf.placeholder(tf.float32, [None, FLAGS.seq_length, 32, 32, 3])
+
+    # possible dropout inside
+    keep_prob = tf.placeholder("float")
+    x_dropout = tf.nn.dropout(x, keep_prob)
+
+    # create network
+    x_unwrap = []
+
+    # conv peice in
+    for i in xrange(FLAGS.seq_length-1):
+      if i == 0:
+        # conv1
+        conv1 = ld.conv_layer(x_dropout[:,i,:,:,:], 3, 2, 8, "encode_1")
+      else:
+        # conv1
+        conv1 = ld.conv_layer(x_1, 3, 2, 8, "encode_1")
+      # conv2
+      conv2 = ld.conv_layer(conv1, 3, 1, 8, "encode_2")
+      # conv3
+      conv3 = ld.conv_layer(conv2, 3, 2, 8, "encode_3")
+      # conv4
+      conv4 = ld.conv_layer(conv3, 1, 1, 4, "encode_4")
+      # conv5
+      conv5 = ld.transpose_conv_layer(conv4, 1, 1, 8, "decode_5")
+      # conv6
+      conv6 = ld.transpose_conv_layer(conv5, 3, 2, 8, "decode_6")
+      # conv7
+      conv7 = ld.transpose_conv_layer(conv6, 3, 1, 8, "decode_7")
+      # x_1 
+      x_1 = ld.transpose_conv_layer(conv7, 3, 2, 3, "decode_8", True) # set activation to linear
+      x_unwrap.append(x_1)
+      # set reuse to true after first go
+      if i == 0:
+        tf.get_variable_scope().reuse_variables()
+
+    # pack them all together 
+    x_unwrap = tf.pack(x_unwrap)
+    x_unwrap = tf.transpose(x_unwrap, [1,0,2,3,4])
+
+    # this part will be used for generating video
+    x_unwrap_gen = []
+    for i in xrange(50):
+      if i == 0:
+        # conv1
+        conv1 = ld.conv_layer(x[:,0,:,:,:], 3, 2, 8, "encode_1")
+      else:
+        # conv1
+        conv1 = ld.conv_layer(x_1_gen, 3, 2, 8, "encode_1")
+      # conv2
+      conv2 = ld.conv_layer(conv1, 3, 1, 8, "encode_2")
+      # conv3
+      conv3 = ld.conv_layer(conv2, 3, 2, 8, "encode_3")
+      # conv4
+      conv4 = ld.conv_layer(conv3, 1, 1, 4, "encode_4")
+      # conv5
+      conv5 = ld.transpose_conv_layer(conv4, 1, 1, 8, "decode_5")
+      # conv6
+      conv6 = ld.transpose_conv_layer(conv5, 3, 2, 8, "decode_6")
+      # conv7
+      conv7 = ld.transpose_conv_layer(conv6, 3, 1, 8, "decode_7")
+      # x_1 
+      x_1_gen = ld.transpose_conv_layer(conv7, 3, 2, 3, "decode_8", True) # set activation to linear
+      x_unwrap_gen.append(x_1_gen)
+   
+    # pack them generated ones
+    x_unwrap_gen = tf.pack(x_unwrap_gen)
+    x_unwrap_gen = tf.transpose(x_unwrap_gen, [1,0,2,3,4])
+
+    # calc total loss (compare x_t to x_t+1)
+    loss = tf.nn.l2_loss(x[:,1:,:,:,:] - x_unwrap)
+    tf.scalar_summary('loss', loss)
+
+    # training
+    train_op = tf.train.AdamOptimizer(FLAGS.lr).minimize(loss)
+    
+    # List of all Variables
+    variables = tf.all_variables()
+
+    # Build a saver
+    saver = tf.train.Saver(tf.all_variables())   
+
+    # Summary op
+    summary_op = tf.merge_all_summaries()
+ 
+    # Build an initialization operation to run below.
+    init = tf.initialize_all_variables()
+
+    # Start running operations on the Graph.
+    sess = tf.Session()
+
+    # init if this is the very time training
+    print("init network from scratch")
+    sess.run(init)
+
+    # Summary op
+    graph_def = sess.graph.as_graph_def(add_shapes=True)
+    summary_writer = tf.train.SummaryWriter(FLAGS.train_dir, graph_def=graph_def)
+
+    for step in xrange(FLAGS.max_step):
+      dat = generate_bouncing_ball_sample(FLAGS.batch_size, FLAGS.seq_length, 32, FLAGS.num_balls)
+      t = time.time()
+      _, loss_r = sess.run([train_op, loss],feed_dict={x:dat, keep_prob:FLAGS.keep_prob})
+      elapsed = time.time() - t
+
+      if step%100 == 0 and step != 0:
+        summary_str = sess.run(summary_op, feed_dict={x:dat, keep_prob:FLAGS.keep_prob})
+        summary_writer.add_summary(summary_str, step) 
+        print("time per batch is " + str(elapsed))
+        print(step)
+        print(loss_r)
+      
+      assert not np.isnan(loss_r), 'Model diverged with loss = NaN'
+
+      if step%1000 == 0:
+        checkpoint_path = os.path.join(FLAGS.train_dir, 'model.ckpt')
+        saver.save(sess, checkpoint_path, global_step=step)  
+        print("saved to " + FLAGS.train_dir)
+
+        # make video
+        print("now generating video!")
+        video = cv2.VideoWriter()
+        success = video.open("generated_conv_video.mov", fourcc, 4, (180, 180), True)
+        dat_gif = dat
+        ims = sess.run([x_unwrap_gen],feed_dict={x:dat_gif, keep_prob:FLAGS.keep_prob})
+        ims = ims[0][0]
+        print(ims.shape)
+        for i in xrange(50):
+          x_1_r = np.uint8(np.maximum(ims[i,:,:,:], 0) * 255)
+          new_im = cv2.resize(x_1_r, (180,180))
+          video.write(new_im)
+        video.release()
+
+
+def main(argv=None):  # pylint: disable=unused-argument
+  if tf.gfile.Exists(FLAGS.train_dir):
+    tf.gfile.DeleteRecursively(FLAGS.train_dir)
+  tf.gfile.MakeDirs(FLAGS.train_dir)
+  train()
+
+if __name__ == '__main__':
+  tf.app.run()
+
+
